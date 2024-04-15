b4d7a53622c22ea21a64c5434acc53a9d39e5733
sashtje <sashtje.lefevre@gmail.com>
2024-04-15 22:11:51 +0300

    fix: fix models and migrate

--- hackernews/hackerposts/models.py
+++ hackernews/hackerposts/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class Author(models.Model):
     nickname = models.CharField(max_length=25, unique=True)
 
+    def __str__(self):
+        return self.nickname
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -14,11 +21,33 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     url = models.URLField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['-created_at'])
+        ]
+
 
 class Comment(models.Model):
     author = models.ForeignKey('Author', on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     text = models.TextField(blank=True)
-    root_comment_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    parent_comment_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    rootcomment = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True, blank=True, related_name='root')
+    parentcomment = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True, blank=True, related_name='parent')
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+        ordering = ['created_at']
+        indexes = [
+            models.Index(fields=['created_at'])
+        ]
