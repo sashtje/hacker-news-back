ef5ec36585cee56dfcf2cca86d99415a05735710
sashtje <sashtje.lefevre@gmail.com>
2024-04-15 23:03:57 +0300

    feat: set up admin panel and add some records to db

--- hackernews/hackerposts/admin.py
+++ hackernews/hackerposts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Post, Comment
+
+per_page = 10
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_per_page = per_page
+    list_display = ('id', 'nickname')
+    list_display_links = ('id', 'nickname')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_per_page = per_page
+    list_display = ('id', 'title', 'author', 'rating', 'created_at', 'url')
+    list_display_links = ('id', 'title')
+    ordering = ['created_at', 'title']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_per_page = per_page
+    list_display = ('id', 'author', 'text','created_at')
+    list_display_links = ('id', 'author', 'text','created_at')
--- hackernews/hackerposts/apps.py
+++ hackernews/hackerposts/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class HackerpostsConfig(AppConfig):
+    verbose_name = 'Hacker news data'
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hackerposts'
